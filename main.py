28a660d129bf3f37cc85b188e88ecf2d02b06fdd
Austin Adamson <austin@ordinaryengineer.com>
2020-08-13 23:57:41 -0700

    I failed at the fibinocci spiral

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import turtle;
 
-def turtleInit():
-    s = turtle.getscreen()
-    t = turtle.Turtle()
-    t.forward(100)
-    t.right(90)
-    turtle.Screen().exitonclick();
+class TurtleDrawing:
+
+    def __init__(self):
+        self.screen = turtle.getscreen();
+        self.t = turtle.Turtle();
+        self.initializeTurtle()
+        turtle.Screen().exitonclick();
+
+    def initializeTurtle(self):
+        self.t.speed(20)
+        previous = 0
+        current = 1
+
+        for x in range(20):
+            self.square(current)
+            self.t.left(90)
+            next = current + previous
+            previous = current
+            current = next
+
+    def square(self, length):
+        for i in range(4):
+            self.t.forward(length)
+            self.t.right(90)
+        return
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    turtleInit()
+    TurtleDrawing()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
