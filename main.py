5e062f9e42022da5999b3bfefdf2062d479269db
Austin Adamson <austin@ordinaryengineer.com>
2020-08-13 23:21:06 -0700

    Well Turtle Works
    This took me a while because I installed linux and then installed python.... linux doesn't work as simply as i liked. It was weirdly annoying.

---
+++ main.py
@@ -0,0 +1,14 @@
+import turtle;
+
+def turtleInit():
+    s = turtle.getscreen()
+    t = turtle.Turtle()
+    t.forward(100)
+    t.right(90)
+    turtle.Screen().exitonclick();
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    turtleInit()
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
